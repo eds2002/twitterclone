2d3c63efd4a11cd751bba249d6e41996004af780
eds2002 <es23jr@gmail.com>
2023-03-23 13:04:23 -0400

    font sizing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,7 +28,7 @@ const PostsView = (props: PostWithUser) => {
             post.createdAt
           ).fromNow()}`}</span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-2xl">{post.content}</span>
       </div>
     </div>
   );
