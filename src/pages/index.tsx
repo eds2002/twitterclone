1931e63480ec27cdf06642ad2ceb9b394f713ebc
eds2002 <es23jr@gmail.com>
2023-03-23 13:56:23 -0400

    New files for new routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ import { api, RouterOutputs } from "~/utils/api";
 import LoadingSpinner, { LoadingPage } from "~/components/LoadingSpinner";
 import { useState } from "react";
 import toast from "react-hot-toast";
+import Link from "next/link";
 
 type PostWithUser = RouterOutputs["posts"]["getAll"][number];
 const PostsView = (props: PostWithUser) => {
@@ -25,10 +26,14 @@ const PostsView = (props: PostWithUser) => {
       />
       <div className="flex flex-col">
         <div className="text-slate-300">
-          <span>{`@${author.username}`}</span>{" "}
-          <span className="font-thin">{` * ${dayjs(
-            post.createdAt
-          ).fromNow()}`}</span>
+          <Link href={`/@${author.username}`}>
+            <span>{`@${author.username}`}</span>{" "}
+          </Link>
+          <Link href={`/post/${post.id}`}>
+            <span className="font-thin">{` * ${dayjs(
+              post.createdAt
+            ).fromNow()}`}</span>
+          </Link>
         </div>
         <span className="text-2xl">{post.content}</span>
       </div>
